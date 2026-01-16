96a6ce9ec82b8c56424f282b75adde0dcac6feb1
gxickx <mgrokoe@gmail.com>
2026-01-16 16:32:51 -0300

    Agrego la dependencia de JPA, creo el repositorio de Usuario y modifico la clase para que sea compatible con JPA

--- src/main/java/org/mgroko/programa/modelo/Usuario.java
+++ src/main/java/org/mgroko/programa/modelo/Usuario.java
@@ -20,12 +20,12 @@ public class Usuario {
 
     // todo VERIFICAR LA LONGITUD DEL usuario y contraseña
 
-    @NotBlank(message = "La dirección es obligatoria")
+    @NotBlank(message = "El nombre de usuario es obligatorio")
     @Size(min = 5, max = 30, message = "El usuario debe tener entre 5 y 30 caracteres")
     @Column(name = "username", nullable = false, length = 30)
     private String username;
 
-    @NotBlank(message = "La dirección es obligatoria")
+    @NotBlank(message = "La contraseña es obligatoria")
     @Size(min = 6, max = 30, message = "La contraseña debe tener entre 5 y 30 caracteres")
     @Column(name = "password", nullable = false, length = 30)
     private String password;
---
+++ src/main/java/org/mgroko/programa/repositorios/UsuarioRepositorio.java
@@ -0,0 +1,13 @@
+package org.mgroko.programa.repositorios;
+
+import org.mgroko.programa.modelo.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByUsername(String username);
+}
