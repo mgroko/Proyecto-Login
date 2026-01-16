a7a706bb4e82b28f633fe3005d4eaac704c72ef8
gxickx <mgrokoe@gmail.com>
2026-01-16 16:42:27 -0300

    Empiezo a desarrollar el servicio del Login, con la lógica para autentificar si el usuario ingresado está en la base de datos. Agrego al controlador de la pantalla de login un try-catch para mostrar el mensaje si el usuario o contraseña se ingresó mal. Incorporo un comentario para recordar que se puede mejorar con JS muchas funcionalidades de las pantallas.

--- src/main/java/org/mgroko/programa/controladores/LoginControlador.java
+++ src/main/java/org/mgroko/programa/controladores/LoginControlador.java
@@ -1,10 +1,27 @@
 package org.mgroko.programa.controladores;
 
+import ch.qos.logback.core.model.Model;
+import org.mgroko.programa.modelo.Usuario;
+import org.mgroko.programa.servicio.LoginServicio;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginControlador {
 
+    private LoginServicio loginServicio;
+    public LoginControlador(LoginServicio loginServicio) {this.loginServicio = loginServicio;}
 
+@PostMapping("/login")
+    public String inicioSesion(@ModelAttribute Usuario usuario) {
+        try {
+            loginServicio.iniciarSesion(usuario.getUsername(), usuario.getPassword());
+            return "redirect:/login";
+        } catch (Exception e) {
+            return "redirect:/login" + "?error=" + e.getMessage();
+        }
+
+}
 
 }
--- src/main/java/org/mgroko/programa/servicio/LoginServicio.java
+++ src/main/java/org/mgroko/programa/servicio/LoginServicio.java
@@ -1,11 +1,38 @@
 package org.mgroko.programa.servicio;
 
+import org.mgroko.programa.modelo.Usuario;
+import org.mgroko.programa.repositorios.UsuarioRepositorio;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 
 public class LoginServicio {
 
+    private final UsuarioRepositorio usuarioRepositorio;
 
+    public LoginServicio(UsuarioRepositorio usuarioRepositorio) {
+        this.usuarioRepositorio = usuarioRepositorio;
+    }
+
+public Usuario iniciarSesion(String user, String passwordIngresada) {
+
+    Optional<Usuario> userOpt = usuarioRepositorio.findByUsername(user);
+
+    if (userOpt.isEmpty()){
+        throw new IllegalArgumentException("Usuario no encontrado");
+    }
+
+    Usuario usuario = userOpt.get();
+
+    if (!usuario.getPassword().equals(passwordIngresada)){
+        throw new IllegalArgumentException("Contraseña incorrecta");
+    }
+
+    System.out.println("LO LOGRE");
+    return usuario;
+
+}
 
 }
